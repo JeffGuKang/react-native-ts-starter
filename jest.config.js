3a82ef8d3c5849888cb93a10e985fa58fc6e1a40
Jeff Gu Kang <jeffgukang@gmail.com>
2019-06-05 22:44:22 +0900

    Init

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'react-native',
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    '^.+\\.(ts|tsx)?$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.jest.json'
+    }
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  setupFiles: [],
+  cacheDirectory: '.jest/cache'
+};
